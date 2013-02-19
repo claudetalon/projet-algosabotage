e689a3f0c6cbcce36b523f24ede0ed0c93d955dd
kimld2590 <kld2590am@gmail.com>
2013-02-19 16:05:34 +0100

    commit main

---
+++ main.py
@@ -0,0 +1,67 @@
+'''
+Created on 19 févr. 2013
+
+@author: kim
+'''
+
+'''
+class OmnicientBlocker:
+def play(position, graph)
+
+class RandomBlocker:
+def play(position, graph)
+-> graphe modifié
+
+class Runner:
+def play(graph, currentPosition, goal)
+-> return sommet ou -1 si perdu
+'''
+
+
+
+class Graphe:
+    def __init__(self):
+        return;
+    
+class Blocker:     
+    def __init__(self): 
+        return;
+    
+    def play(self,position,graphe):
+        res = Graphe()
+        return res;
+
+class Runner:
+    def __init__(self): 
+        return;
+    def play(self,position,graphe,goal):
+        res = -1;
+        return res;
+            
+        
+if __name__ == '__main__':
+    
+    print ('debut main');
+    
+    runner = Runner();
+    blocker = Blocker();
+    graphe = Graphe();
+    goal = 1;
+        
+    run = True;
+    
+    ###
+    while(run):
+        runnerRes = runner.play(0,graphe,1);
+        if(runnerRes == -1 or runnerRes == goal):
+            run=False;
+            if(runnerRes==goal):
+                print('runner win');
+            else:
+                print('runner lose');
+                
+        blocker.play(0, graphe);            
+    ###
+    
+    print ('fin du programme');
+    
