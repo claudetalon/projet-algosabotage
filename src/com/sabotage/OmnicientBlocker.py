e2c1439b4ae67dc1b58d1212b04ae487ba10f35f
Sentmoraap <lilian.gimenez@laposte.net>
2013-02-20 23:38:26 +0100

    Modified OmnicientBlocker to avoid too deep recursions.

--- src/com/sabotage/OmnicientBlocker.py
+++ src/com/sabotage/OmnicientBlocker.py
@@ -1,40 +1,46 @@
 from Blocker import Blocker
 import copy
+from PathsAlgo import find_all_paths
 
 class OmnicientBlocker(Blocker):
     def __init__(self): 
         return
 
     def play(self, position, graph, goal):
-        move=self.blockerPlay(position,graph,goal)
+        move=self.blockerPlay(position,graph,goal, 5, -1000, 1000)
         print('removed'+str(move[1]))
         graph[move[1][0]][move[1][1]]-=1
         return
 
-    # Does a minmax algorithm. -1=runner wins, 1=blocker wins
+    # Does a minmax algorithm. -1000=runner wins, 1000=blocker wins
     # return value = (win/lose,edge to remove)
-    def blockerPlay(self, position, graph, goal):
-        score=1
+    def blockerPlay(self, position, graph, goal, level, alpha, beta):
         retEdge=(-1,-1)
         for (a,b) in enumerate(graph):
             for (c,d) in enumerate(b):
                 if d>0:
-                    newGraph=copy.deepcopy(graph)
-                    newGraph[a][c]-=1
-                    score=self.runnerPlay(position,newGraph,goal)
-                    if score==1:
-                        return (1,(a,c))
-                    retEdge=(a,c)
-        return (score,retEdge)
+                    graph[a][c]-=1
+                    score=self.runnerPlay(position,graph,goal,level,alpha,beta)
+                    graph[a][c]+=1
+                    if score>alpha:
+                        alpha=score
+                        retEdge=(a,c)
+                        if alpha>=beta:
+                            return(alpha,retEdge)
+        return (alpha,retEdge)
 
-    def runnerPlay(self, position,graph, goal):
-        score=1
+    def runnerPlay(self, position,graph, goal, level, alpha, beta):
         for (a,b) in enumerate(graph[position]):
             if b>0:
                 if a==goal:
-                    return -1
+                    return -1000
                 else:
-                    score=self.blockerPlay(a,graph,goal)[0]
-                    if score==-1:
-                        return -1
-        return score
+                    if level==0:
+                        score=-1000+len(find_all_paths(graph, a, goal))
+                    else:
+                        score=self.blockerPlay(a,graph,goal,level-1,alpha,beta)[0]
+                    if score<beta:
+                        beta=score
+                        if alpha>=beta:
+                            return beta
+        return min
